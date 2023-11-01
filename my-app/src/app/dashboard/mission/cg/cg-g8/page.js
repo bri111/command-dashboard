71b134c5de4d42d2173ba86273673aab51584cc6
Marissa Halim <halimmq2@gmail.com>
2023-11-01 13:15:35 -1000

    bar chart works

--- my-app/src/app/dashboard/mission/cg/cg-g8/page.js
+++ my-app/src/app/dashboard/mission/cg/cg-g8/page.js
@@ -1,14 +1,18 @@
 import React, { useState, useRef, useEffect } from 'react';
 import Papa from 'papaparse';
-import { Bar } from 'react-chartjs-2';
+import * as d3 from 'd3';
 
 export const CG_G8 = () => {
 
-  // const svgRef = useRef();
+  const svgRef = useRef();
+
+  let labels = [];
+  let ADOS = [];
+  let title = "";
+  let dataArray = [];
+  let cleanData = [];
 
   const [values, setValues] = useState([]);
-  const [ados, setAdos] = useState([]);
-  const [xAxisVals, setXAxisVals] = useState([]);
 
   const parseCsv = (event) => {
     const csvFile = event.target.files[0];
@@ -25,20 +29,88 @@ export const CG_G8 = () => {
         });
 
         setValues(valuesArray);
-        console.log(values);
-
-        setXAxisVals(valuesArray[0]);
-        console.log(xAxisVals);
-
-        setAdos(valuesArray[1]);
-        console.log(ados);
-
+        // console.log(values);
       }
-
     });
   }
 
+  ////////  data code  ////////
+  useEffect(() => {
+    // slice data
+    labels = values[0];
+    if (labels) {
+      labels = labels.slice(1);
+    }
 
+    ADOS = values[1];
+    if (ADOS) {
+      ADOS = ADOS.slice(1);
+      for (let a = 0; a < ADOS.length; a++) {
+        ADOS[a] = parseFloat(ADOS[a].substring(2));
+        console.log(ADOS[a]);
+      }
+    }
+    // title = values[1].slice(0, 1);
+    if (dataArray && labels) {
+      dataArray = labels.map(function (value, index) {
+        return [value, ADOS[index]]
+      });
+    }
+
+    if (cleanData) {
+      cleanData = dataArray.map(index => (
+        {
+          label: index[0],
+          value: index[1]
+        }
+      ));
+    }
+
+    // console.log(labels);
+    console.log(ADOS);
+    console.log(dataArray);
+    console.log(cleanData);
+  });
+
+  ////////  d3 code  ////////
+  useEffect(() => {
+    // set up container
+    const w = 600;
+    const h = 600;
+    const svg = d3.select(svgRef.current)
+      .attr('width', w)
+      .attr('height', h)
+      .style('overflow', 'visible');
+
+    // set up X axis
+    const xScale = d3.scaleBand()
+      .domain(cleanData.map(d => d.label))
+      .range([0, w])
+      .padding(0.5);
+    const xAxis = d3.axisBottom(xScale)
+      .ticks(cleanData.length);
+    svg.append('g')
+      .call(xAxis)
+      .attr("transform", `translate(0, ${h})`)
+
+    // set up Y axis
+    const yScale = d3.scaleLinear()
+      .domain([0, 70])
+      .range([h, 0]);
+    const yAxis = d3.axisLeft(yScale);
+    svg.append('g')
+      .call(yAxis);
+
+    svg.selectAll('.bar')
+      .data(cleanData)
+      .join('rect')
+      .attr("x", d => xScale(d.label))
+      .attr("y", d => yScale(d.value))
+      .attr("width", xScale.bandwidth())
+      .attr("height", d => h - yScale(d.value))
+      .attr("fill", "#69b3a2")
+
+  }, [cleanData]);
 
   return (
     <div className="container">
@@ -57,10 +129,7 @@ export const CG_G8 = () => {
         file:bg-gray-200 file:text-gray-700
         hover:file:bg-gray-300'
       />
-      <div id="chart" className="ml-auto mr-auto text-center">
-        {/* <svg ref={svgRef}></svg> */}
-        {/* <Bar data={ados} /> */}
-      </div>
+      <svg ref={svgRef}></svg>
     </div>
   )
 }
