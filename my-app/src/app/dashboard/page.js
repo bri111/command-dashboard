03021f047066bd5d131af45396bd61eea7e4f4de
Marissa Halim <halimmq2@gmail.com>
2023-11-08 15:36:03 -1000

    update home page

--- my-app/src/app/dashboard/page.js
+++ my-app/src/app/dashboard/page.js
@@ -5,37 +5,24 @@ import Image from 'next/image';
 // import {Card, CardBody } from "@material-tailwind/react";
 
 const Page = async () => {
-  const cardInfo = [
-    { name: 'Mission', href: '/dashboard/mission' },
-    { name: 'Periodic', href: '/dashboard/periodic' },
-    { name: 'Monthly', href: '/dashboard/monthly' },
-  ]
+
   const data = await prisma.mytable.findMany();
   console.log(data);
   return (
-    <div className="bg-white  px-6 py-8 sm:py-12 lg:px-8 h-screen align-middle">
-      <div>
-        <Image
-          src="/9th_banner2.png"
-          width={1500}
-          height={500}
-          alt="Picture of the author"
-        />
-      </div>
-      <div className="py-8 mx-auto max-w2xl flex flex-row justify-evenly">
-        {
-          cardInfo.map((e) => (
-            <a key={e.name} href={e.href} className="max-w-sm rounded overflow-hidden shadow-lg">
-              <div className="px-6 py-4">
-                <div className="font-bold text-xl mb-2 text-center">{e.name}</div>
-                <p className="text-gray-700 text-base">
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
-                </p>
-              </div>
-            </a>
-          )
-          )
-        }
+    <div className="bg-white px-6 py-8 sm:py-4 lg:px-8 h-screen align-middle">
+      <Image
+        src="/9th_banner2.png"
+        width={1500}
+        height={500}
+        alt="Picture of the author"
+      />
+      <div className="container mx-auto pt-8">
+        <h1 className='text-2xl text-center p-2'>
+          Mission
+        </h1>
+        <p className='text-center'>
+          The 9th MSC, as an operational reserve in the USINDOPACOM Area of Operations, delivers key reserve component capabilities, maintains a forward posture, is prepared to rapidly mobilize, and provides command and control for Homeland Defense/DSCA, enabling the Theater Army to assure allies, compete with adversaries, respond in crisis, and win in conflict.
+        </p>
       </div>
     </div>
   );
