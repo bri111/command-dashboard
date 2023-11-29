92e8f67f157eb654fd87a5f8b052321ee83036d4
Marissa Halim <halimmq2@gmail.com>
2023-11-29 00:25:57 -1000

    carousel for homepg

--- my-app/src/app/dashboard/page.js
+++ my-app/src/app/dashboard/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import db from '@/modules/db';
 import prisma from '@/modules/db';
 import Image from 'next/image';
+import Footer from '@/components/Footer';
 // import {Card, CardBody } from "@material-tailwind/react";
 
 const Page = async () => {
@@ -21,14 +22,54 @@ const Page = async () => {
         height={500}
         alt="Picture of the author"
       />
-      <div className="container mx-auto pt-8">
+      <div className="container mx-auto">
         <h1 className='text-2xl text-center p-2'>
           Mission
         </h1>
         <p className='text-center'>
           The 9th MSC, as an operational reserve in the USINDOPACOM Area of Operations, delivers key reserve component capabilities, maintains a forward posture, is prepared to rapidly mobilize, and provides command and control for Homeland Defense/DSCA, enabling the Theater Army to assure allies, compete with adversaries, respond in crisis, and win in conflict.
         </p>
+        <div className='container pt-4'>
+          <div className="carousel w-full">
+            <div id="slide1" className="carousel-item relative w-full">
+              <img src="/9MSC-1.JPG" className="w-full flex justify-center items-center" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide5" className="btn btn-circle">❮</a>
+                <a href="#slide2" className="btn btn-circle">❯</a>
+              </div>
+            </div>
+            <div id="slide2" className="carousel-item relative w-full">
+              <img src="/9MSC-2.JPG" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide1" className="btn btn-circle">❮</a>
+                <a href="#slide3" className="btn btn-circle">❯</a>
+              </div>
+            </div>
+            <div id="slide3" className="carousel-item relative w-full">
+              <img src="/9MSC-3.JPG" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide2" className="btn btn-circle">❮</a>
+                <a href="#slide4" className="btn btn-circle">❯</a>
+              </div>
+            </div>
+            <div id="slide4" className="carousel-item relative w-full">
+              <img src="/9MSC-4.JPG" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide3" className="btn btn-circle">❮</a>
+                <a href="#slide5" className="btn btn-circle">❯</a>
+              </div>
+            </div>
+            <div id="slide5" className="carousel-item relative w-full">
+              <img src="/9MSC-5.JPG" className="w-full" />
+              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                <a href="#slide4" className="btn btn-circle">❮</a>
+                <a href="#slide1" className="btn btn-circle">❯</a>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 }
