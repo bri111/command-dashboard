96844f59f5ce2e3c5970787fba96250a58280c70
bri111 <brianall@hawaii.edu>
2024-06-06 19:57:39 -1000

    added pbix embed

--- my-app/src/app/dashboard/page.js
+++ my-app/src/app/dashboard/page.js
@@ -30,44 +30,38 @@ const Dashboard = async () => {
         <div className='container pt-4'>
           <div className="carousel w-full">
             <div id="slide1" className="carousel-item relative w-full flex justify-center items-center">
-              <img src="/9MSC-6.JPG" className="w-1/3" />
+              <img src="/9MSC-6.JPG" className="w-1/3"/>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide5" className="btn btn-circle">❮</a>
                 <a href="#slide2" className="btn btn-circle">❯</a>
               </div>
             </div>
             <div id="slide2" className="carousel-item relative w-full flex justify-center items-center">
-              <img src="/9MSC-1.JPG" className="w-1/2" />
+              <img src="/9MSC-1.JPG" className="w-1/2"/>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide1" className="btn btn-circle">❮</a>
                 <a href="#slide3" className="btn btn-circle">❯</a>
               </div>
             </div>
             <div id="slide3" className="carousel-item relative w-full flex justify-center items-center">
-              <img src="/9MSC-2.JPG" className="w-1/2" />
+              <img src="/9MSC-2.JPG" className="w-1/2"/>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide2" className="btn btn-circle">❮</a>
                 <a href="#slide4" className="btn btn-circle">❯</a>
               </div>
             </div>
             <div id="slide4" className="carousel-item relative w-full flex justify-center items-center">
-              <img src="/9MSC-4.JPG" className="w-1/2" />
+              <img src="/9MSC-4.JPG" className="w-1/2"/>
               <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                 <a href="#slide3" className="btn btn-circle">❮</a>
                 <a href="#slide5" className="btn btn-circle">❯</a>
               </div>
             </div>
-            <div id="slide5" className="carousel-item relative w-full flex justify-center items-center">
-              <img src="/9MSC-5.JPG" className="w-1/2" />
-              <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-                <a href="#slide4" className="btn btn-circle">❮</a>
-                <a href="#slide1" className="btn btn-circle">❯</a>
-              </div>
-            </div>
+
           </div>
         </div>
       </div>
-    <Footer />
+      <Footer/>
     </div>
   );
 }
--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -10,9 +10,20 @@ import NavContext from 'react-bootstrap/NavContext';
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Page />
-      <Footer />
+      {/*<Navbar />*/}
+      <Page/>
+      <div className="flex justify-center items-center min-h-screen">
+        <iframe
+          title="testingCD"
+          width="1280"
+          height="650"
+          src="https://app.powerbi.com/reportEmbed?reportId=261fcbeb-1779-44a5-8873-da367cfc7fa9&autoAuth=true&ctid=612bb818-ec38-4e9b-884d-124123cd922c"
+          frameBorder="0"
+          allowFullScreen="true"
+        ></iframe>
+      </div>
+
+      <Footer/>
     </>
   );
 }
